9d6cd084be296fc6f1b793ca6a79419dcccb2cc0
kelseylw1822 <kelseylw1822@gmail.com>
2025-02-03 12:16:22 -0500

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Kelsey Wessel on 2/3/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
